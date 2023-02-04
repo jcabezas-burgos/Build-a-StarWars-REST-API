2236a70e51aa3ef50c2baee93d51b06f54d9f548
Jcabezas.burgos <jcabezas.burgos@gmail.com>
2023-02-04 18:04:26 +0000

    trabajo

--- src/app.py
+++ src/app.py
@@ -1,51 +1,178 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
-from flask_swagger import swagger
 from flask_cors import CORS
-from utils import APIException, generate_sitemap
-from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Character, Favorite, Planet, Vehicle
+
+
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASEDIR,"swapi.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["ENV"] = "development"
 
-db_url = os.getenv("DATABASE_URL")
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
-setup_admin(app)
+Migrate(app, db)
 
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
+@app.route("/")
+def home():
+    return "prueba exitosa"
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route("/people", methods=["GET"])
+def people():
+    character = Character.query.all()
+    character_serialized = list(map( lambda character: character.serialize(), character))
+    return jsonify(character_serialized)
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route("/character/<int:id>", methods=["GET"])
+def character(id):
+    character = Character.query.get(id)
+    character_serialized = character.serialize()
+    return jsonify(character_serialized)
 
-    return jsonify(response_body), 200
+@app.route("/add_character", methods=["POST"])
+def add_character():
+    character = Character()
+    character.name = request.json.get("name")
+    character.hair_color = request.json.get("hair_color")
+    character.birth_year = request.json.get("birth_year")
 
-# this only runs if `$ python src/app.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    db.session.add(character)
+    db.session.commit()
+
+    return jsonify({
+        "msg": "personaje añadido correctamente"
+        }), 200
+
+@app.route("/planets", methods=["GET"])
+def planets():
+    planet = Planet.query.all()
+    planet_serialized = list(map( lambda planet: planet.serialize(), planet))
+    return jsonify(planet_serialized)
+
+@app.route("/planets/<int:id>", methods=["GET"])
+def planet(id):
+    planet = Planet.query.get(id)
+    planet_serialized = planet.serialize()
+    return jsonify(planet_serialized)
+
+@app.route("/vehicles", methods=["GET"])
+def vehicles():
+    vehicle = Vehicle.query.all()
+    vehicle_serialized = list(map( lambda vehicle: vehicle.serialize(), vehicle))
+    return jsonify(vehicle_serialized)
+
+@app.route("/vehicles/<int:id>", methods=["GET"])
+def vehicle(id):
+    vehicle = Vehicle.query.get(id)
+    vehicle_serialized = vehicle.serialize()
+    return jsonify(vehicle_serialized)
+
+
+@app.route("/users", methods=["GET"])
+def users():
+    user = User.query.all()
+    user_serialized = list(map( lambda user: user.serialize(), user))
+    return jsonify(user_serialized)
+
+@app.route("/users/favorites/<int:user_id>", methods=["GET"])
+def user_favorites(user_id):
+    user= User.query.get(user_id)
+    favorite = Favorite.query.filter_by(user_id=user_id).all()
+    favorite_serialized = list(map( lambda favorite: favorite.serialize(), favorite))
+    return jsonify(favorite_serialized)
+
+@app.route("/favorite/planet/<int:planet_id>", methods=["POST"])
+def add_favorite_planet(planet_id):
+    favorite = Favorite()
+    user_id = request.json.get("user_id")
+
+    found_user = Favorite.query.filter_by(user_id=user_id).all()
+
+    if found_user is None:
+        return jsonify({
+            "msg": "no existe este usuario"
+        }), 400
+
+    favorite.planet_id = request.json.get("planet_id")
+    favorite.user_id = user_id
+
+    db.session.add(favorite)
+    db.session.commit()
+
+    return jsonify({
+        "msg": "favorito añadido correctamente"
+    }), 200
+
+@app.route("/favorite/people/<int:character_id>", methods=["POST"])
+def add_favorite_character(character_id):
+    favorite = Favorite()
+    user_id = request.json.get("user_id")
+
+    found_user = Favorite.query.filter_by(user_id=user_id).all()
+
+    if found_user is None:
+        return jsonify({
+            "msg": "no existe este usuario"
+        }), 400
+
+    favorite.character_id = request.json.get("character_id")
+    favorite.user_id = user_id
+
+    db.session.add(favorite)
+    db.session.commit()
+
+
+    return jsonify({
+        "msg": "favorito añadido correctamente"
+    }), 200
+
+@app.route("/favorite/planet/<int:planet_id>/", methods=["DELETE"])
+def delete_favorite_planet(planet_id):
+    favorite = Favorite.query.get(planet_id)
+    
+    db.session.delete(favorite)
+    db.session.commit()
+
+    return "favorito eliminado correctamente"
+
+@app.route("/favorite/people/<int:character_id>/", methods=["DELETE"])
+def delete_favorite_character(character_id):
+    favorite = Favorite.query.get(character_id)
+    
+    db.session.delete(favorite)
+    db.session.commit()
+
+    return "favorito eliminado correctamente"
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    user = User()
+    email = request.json.get("email")
+    password = request.json.get("password")
+
+    found_email = User.query.filter_by(email=email).first()
+
+    if found_email is not None:
+        return jsonify({
+            "msg": "Ya existe un usuario ingresado con este email"
+        }), 400
+
+    user.name = request.json.get("name")
+    user.email = email
+    user.password = password
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({
+        "msg": "usurario añadido correctamente"
+        }), 200
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=34025)
--- src/models.py
+++ src/models.py
@@ -2,18 +2,88 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(250))
+    password = db.Column(db.String(10))
+    favorites = db.relationship("Favorite")
+    
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    __tablename__ = "characters"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    hair_color = db.Column(db.String(100))
+    birth_year = db.Column(db.String(100))
+    planet = db.relationship("Planet", back_populates="character", uselist=False)
+    vehicle = db.relationship("Vehicle", back_populates="character", uselist=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "hair_color": self.hair_color,
+            "birth_year": self.birth_year,
+            "planet": self.planet,
+            "vehicle": self.vehicle,
+        }
+
+class Planet(db.Model):
+    __tablename__ = "planets"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    population = db.Column(db.Integer)
+    character_id = db.Column (db.Integer, db.ForeignKey("characters.id"))
+    character = db.relationship("Character", back_populates="planet")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "character_id": self.character_id
+        }
+
+
+class Vehicle(db.Model):
+    __tablename__ = "vehicles"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    crew = db.Column(db.Integer)
+    character_id = db.Column(db.Integer, db.ForeignKey("characters.id"))
+    character = db.relationship("Character", back_populates="vehicle")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "crew": self.population,
+            "character_id": self.character_id
+        }
+
+class Favorite(db.Model):
+    __tablename__ = "favorites"
+    id = db.Column(db.Integer, primary_key = True)
+    planet_id = db.Column(db.Integer, db.ForeignKey("planets.id")) 
+    character_id = db.Column(db.Integer, db.ForeignKey("characters.id")) 
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False) 
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet_id": self.planet_id,
+            "character_id": self.character_id,
+            "user_id": self.user_id,
         }
